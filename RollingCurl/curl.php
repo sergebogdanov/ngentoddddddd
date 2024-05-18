9d8544dd82acecdbe12dd8752eb66e8e881efc8d
sergebogdanov <sergebogdanov161@gmail.com>
2024-05-18 03:57:34 +0000

    Update curl.php

--- RollingCurl/curl.php
+++ RollingCurl/curl.php
@@ -1,16 +1,5 @@
 class MyCurl {
 	var $ch, $agent, $error, $info, $cookiefile, $savecookie;	
-	function MyCurl() {
-		$this->agent = $this->get_agent(rand(0,44));
-		$this->ch = curl_init();
-		curl_setopt ($this->ch, CURLOPT_USERAGENT, $this->agent);
-		curl_setopt ($this->ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt ($this->ch, CURLOPT_SSL_VERIFYPEER, 0);
-//	curl_setopt ($this->ch, CURLOPT_SSL_VERIFYHOST, 1);
-		curl_setopt ($this->ch, CURLOPT_FOLLOWLOCATION,true);
-		curl_setopt ($this->ch, CURLOPT_TIMEOUT, 20);
-		curl_setopt ($this->ch, CURLOPT_CONNECTTIMEOUT,20);
-		}
 	function http_code() {
     	return curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
   	}
